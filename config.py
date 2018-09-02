db0b91d9cce3e773cfb02d230a01e3931c8c9b9b
ctcodingstyle <ctcodingstyle@gmail.com>
2018-09-02 23:27:26 +0800

    #1 项目结构搭建

---
+++ config.py
@@ -0,0 +1,12 @@
+DEBUG = True
+
+DB_USERNAME = 'root'
+DB_PASSWORD = 'root'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'blog'
+
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
